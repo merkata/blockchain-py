c3ed1f8e237fc91e47f3a23e642013414342174c
Mariyan Dimitrov <mddimitrov@telenor.bg>
2017-11-07 16:08:38 +0200

    Added API part

--- blockchain.py
+++ blockchain.py
@@ -2,6 +2,8 @@ import hashlib
 import json
 from time import time
 from uuid import uuid4
+from textwrap import dedent
+from flask import Flask, jsonify, request
 
 
 class Blockchain(object):
@@ -97,3 +99,72 @@ class Blockchain(object):
     def last_block(self):
         # Return the last Block
         pass
+
+
+# API starts here!
+
+# Create the Flask app
+app = Flask(__name__)
+
+# Unique node identifier
+node_identifier = str(uuid4().replace("-", ""))
+
+# New Blockchain
+blockchain = Blockchain()
+
+
+# Flask routes
+@app.route('/mine', methods=['GET'])
+def mine():
+    # Get the next proof from the POW algo
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transaction(
+            sender=0,
+            recipient=node_identifier,
+            amount=1
+            )
+
+    block = blockchain.new_block(proof)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+
+    return jsonify(response), 200
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+    required = ['sender', 'recipient', 'amount']
+
+    if not all(k in values for k in required):
+        return "Missing values", 400
+
+    index = blockchain.new_transaction(
+            values.sender, values.recipient, values.amount)
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+
+    return jsonify(response), 200
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'blockchain': blockchain.chain,
+        'length': len(blockchain.chain)
+        }
+
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
